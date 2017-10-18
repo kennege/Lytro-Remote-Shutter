7bea96b050ad9024182e297f8fe5673f18637ff7
kennege <gerardkennedy93@gmail.com>
2017-10-19 10:40:24 +1100

    Add files via upload

---
+++ LytroRemoteShutter/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LytroGerard")]
+[assembly: AssemblyDescription("Lytro compatible library assembled for Gerard Kennedy")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UAM.Optics.LightField.Lytro")]
+[assembly: AssemblyCopyright("Copyright © United Association of Miloushs 1993–2017")]
+[assembly: AssemblyTrademark("Lytro is a trademark of Lytro, Inc., USA. This software is not affiliated with or endorsed by Lytro, Inc.")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
